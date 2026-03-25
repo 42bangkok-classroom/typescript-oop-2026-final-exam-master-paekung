5877d9da70bf1671c83ac577e9c83b0405d6a2d3
paekung <68011088@kmitl.ac.th>
2026-03-25 11:12:07 +0700

    test

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,20 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiResponse } from './interfaces/response.interface';
+
+
+export class Test {
+  service: string;
+  version: string;
+}
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHello(): Promise<ApiResponse<Test>> {
+    const data = {service:'test' , version : 'test'};
+    return { success: true, data , message : "Hello NestJS"};
   }
 }
